ed079484110b45d012d02b705064921798cd3f49
raystatic <rahul9650ray@gmail.com>
2021-01-16 15:00:20 +0530

    login layout

--- app/src/main/java/com/ssjit/papertrading/LoginActivity.kt
+++ app/src/main/java/com/ssjit/papertrading/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.ssjit.papertrading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssjit.papertrading.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
