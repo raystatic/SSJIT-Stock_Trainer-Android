902cf62b36c6be5f5c65f3ae855bb6b8c5d6bf1e
raystatic <rahul9650ray@gmail.com>
2021-01-16 15:30:28 +0530

    google login integrated

---
+++ app/src/main/java/com/ssjit/papertrading/Constants.kt
@@ -0,0 +1,9 @@
+package com.ssjit.papertrading
+
+object Constants {
+
+    const val SOMETHING_WENT_WRONG = "Something went wrong"
+    const val LOGIN_FAILED = "Google login failed"
+    const val CANNOT_LOGIN = "Cannot login at the moment"
+
+}
--- app/src/main/java/com/ssjit/papertrading/LoginActivity.kt
+++ app/src/main/java/com/ssjit/papertrading/LoginActivity.kt
@@ -1,19 +1,84 @@
 package com.ssjit.papertrading
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.ssjit.papertrading.ViewExtension.showSnack
 import com.ssjit.papertrading.databinding.ActivityLoginBinding
+import timber.log.Timber
 
+
+private const val RC_SIGN_IN = 100
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var gso : GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        account?.let { acc->
+            updateUI(acc)
+        } ?: kotlin.run {
+            binding.cardLogin.setOnClickListener {
+                signIn()
+            }
+        }
 
     }
+
+    private fun signIn() {
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun updateUI(acc: GoogleSignInAccount) {
+        Timber.d("login: $acc")
+        binding.root.showSnack(message = "Login success")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+
+            account?.let {
+                updateUI(it)
+            } ?: kotlin.run {
+                binding.root.showSnack(Constants.CANNOT_LOGIN)
+            }
+
+        } catch (e: ApiException) {
+            Timber.d("signInResult:failed code=${e.statusCode}")
+            binding.root.showSnack(Constants.LOGIN_FAILED)
+        }
+    }
+
 }
