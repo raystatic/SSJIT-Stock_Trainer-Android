ec57d6643e405879e3e9302411a6e8bd0d5da4e6
raystatic <rahul9650ray@gmail.com>
2021-01-21 02:46:35 +0530

    stock detail layout

--- app/src/main/java/com/ssjit/papertrading/ui/activities/StockDetailsActivity.kt
+++ app/src/main/java/com/ssjit/papertrading/ui/activities/StockDetailsActivity.kt
@@ -1,12 +1,32 @@
 package com.ssjit.papertrading.ui.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.futured.donut.DonutSection
 import com.ssjit.papertrading.R
+import com.ssjit.papertrading.databinding.ActivityStockDetailsBinding
 
 class StockDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityStockDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stock_details)
+        binding = ActivityStockDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = resources.getColor(R.color.primary_green)
+
+
+        val section1 = DonutSection(
+            name = "section_1",
+            color = resources.getColor(R.color.green),
+            amount = 0.81f
+        )
+
+        binding.donutView.cap = 1f
+        binding.donutView.submitData(listOf(section1))
+
     }
 }
